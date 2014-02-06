a548d5fec6194c875e4375360d0c942c8e22b57f
jacksonkmillsaps <jmillsaps3@gatech.edu>
2014-02-05 22:43:06 -0500

    Added intents to register and login buttons, created registration filler screen,  both buttons take you to their respective screens.  Want to know how to control text on black bar at top of screen.

--- src/com/example/banknote/Welcome_screen.java
+++ src/com/example/banknote/Welcome_screen.java
@@ -1,8 +1,10 @@
 package com.example.banknote;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class Welcome_screen extends Activity {
 
@@ -17,6 +19,20 @@ public class Welcome_screen extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.welcome_screen, menu);
 		return true;
+	}
+	
+	/** Called when the user clicks the Log In button */
+	public void goToLogInScreen(View view) 
+	{
+		Intent intent = new Intent(this, Login_screen.class);
+		startActivity(intent);
+	}
+	
+	/** Called when the user clicks the Register button */
+	public void goToRegisterScreen(View view) 
+	{
+		Intent intent = new Intent(this, Register_screen.class);
+		startActivity(intent);
 	}
 
 }
